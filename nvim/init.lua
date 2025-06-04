edcd5072feb5b70045ff5ffc3b4a76a8b0f6b6fc
Marjan112 <marjanramadanovic1234@gmail.com>
2025-06-04 19:13:26 +0200

    First commit, Yessirski!

---
+++ nvim/init.lua
@@ -0,0 +1,18 @@
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.clipboard = "unnamedplus"
+
+vim.o.termguicolors = true
+
+vim.cmd.colorscheme("sorbet")
+
+-- vim.cmd([[
+--     highlight Normal ctermbg=none guibg=none
+--     highlight NormalNC ctermbg=none guibg=none
+--     highlight EndOfBuffer ctermbg=none guibg=none
+-- ]])
