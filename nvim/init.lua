146b6c403bf6114cc8609d801b3c6be36615e2f9
Marjan112 <marjanramadanovic1234@gmail.com>
2025-09-15 00:04:50 +0200

    i decided to use lazyvim again

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,11 @@
-vim.o.number = true
-vim.o.relativenumber = true
+require("config.lazy")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
+
+vim.o.virtualedit = "all"
 
 vim.o.tabstop = 4
-vim.o.shiftwidth = 4
 vim.o.expandtab = true
-
-vim.o.clipboard = "unnamedplus"
-
-vim.o.termguicolors = true
-
-vim.cmd.colorscheme("habamax")
-
-vim.cmd [[
-    highlight Normal guibg=none
-    highlight NonText guibg=none
-    highlight Normal ctermbg=none
-    highlight NonText ctermbg=none
-]]
-
-require("nvim-tree").setup()
-require("neogit").setup()
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
